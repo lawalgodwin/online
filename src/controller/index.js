ba87a511419bf47c75582d07484a7e6e9474fbdb
godwin <godwinl200@gmail.com>
2019-05-22 16:20:47 +0100

    edited

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,6 +2,7 @@ const boom = require('boom')
 const Product = require('../model/availableCom')
 exports.addItem = async(request, response) => {
     try {
+        console.log(request.body)
         const prod = new Product(request.body)
         return prod.save()
     } catch (error) {
@@ -13,10 +14,8 @@ exports.getall = async(request, response) => {
     var all = []
     try {
         var allitem = await Product.find()
-        allitem.forEach((item, index) => {
-            all.push(`name: ${item.productName}, price: $${item.price | item.z} `)
-        });
-        return (all)
+        console.log(allitem)
+        return allitem
 
     } catch (error) {
         boom.boomify(error)
